5af604446c8110f66e0bf400662838d1ed030b26
mlobeto <mercedeslobeto@gmail.com>
2026-04-16 09:22:30 -0300

    ssssss

--- front/src/Components/Landing.jsx
+++ front/src/Components/Landing.jsx
@@ -1,7 +1,6 @@
 
 import LandingHero from "./LandingHero";
 import LandingFeatures from "./LandingFeatures";
-import LandingPlans from "./LandingPlans";
 import LandingFooter from "./LandingFooter";
 
 const Landing = () => {
@@ -9,7 +8,6 @@ const Landing = () => {
     <div className="landing-container">
        <LandingHero />
        <LandingFeatures />
-       <LandingPlans />
        <LandingFooter />
     </div>
   );
