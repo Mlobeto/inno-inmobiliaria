10b3a236a8166cf44cefe7ba765e1e8f0aa86bd4
mlobeto <mercedeslobeto@gmail.com>
2026-04-16 22:06:12 -0300

    query

--- back/query-trial.js
+++ back/query-trial.js
@@ -1,5 +1,4 @@
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+const prisma = require("./src/utils/prismaClient");
 
 async function main() {
   const user = await prisma.user.findFirst({
